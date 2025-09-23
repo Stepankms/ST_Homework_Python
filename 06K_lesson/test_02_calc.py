5e06452f4a1433a149c3a457758c683550dff274
Stepan <steveandwonder@gmail.com>
2025-09-23 14:34:46 +0300

    Update test_02_calc.py

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -7,24 +7,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
  
-options = webdriver.EdgeOptions()
-driver = webdriver.Edge(options=options)
+def test_calc():
+    options = webdriver.EdgeOptions()
+    driver = webdriver.Edge(options=options)
         
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
-input = driver.find_element(By.CSS_SELECTOR, '#delay')
-input.send_keys('1')
+    input = driver.find_element(By.CSS_SELECTOR, '#delay')
+    input.send_keys('1')
 
-button_7 = driver.find_element(By.XPATH, '//span[text()="7"]').click()
-button_plus = driver.find_element(By.XPATH, '//span[text()="+"]').click()
-button_8 = driver.find_element(By.XPATH, '//span[text()="8"]').click()
-button_equals = driver.find_element(By.XPATH, '//span[text()="="]').click()
-waiter = WebDriverWait(driver, 52)
-waiter.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.screen'), '15'))
+    driver.find_element(By.XPATH, '//span[text()="7"]').click()
+    driver.find_element(By.XPATH, '//span[text()="+"]').click()
+    driver.find_element(By.XPATH, '//span[text()="8"]').click()
+    driver.find_element(By.XPATH, '//span[text()="="]').click()
 
-result = driver.find_element(By.CSS_SELECTOR, 'div.screen').text
-print(result)
+    waiter = WebDriverWait(driver, 52)
+    waiter.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'div.screen'), '15'))
 
-assert result == '15'
+    result = driver.find_element(By.CSS_SELECTOR, 'div.screen').text
+    print(result)
 
-driver.quit
+    assert result == '15'
+
+    driver.quit()
